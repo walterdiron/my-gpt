292f8272c807ba7b510c851f4f20eb5a19a787a3
Walter Diron <walterdiron@gmail.com>
2023-04-23 02:44:26 -0300

    chat py

--- chat.py
+++ chat.py
@@ -1,1 +1,16 @@
-
+import openai
+# Indica el API Key
+openai.api_key = "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"
+# Uso de ChapGPT en Python
+model_engine = "text-davinci-003"
+prompt = "la suma de 5 mas 5"
+completion = openai.Completion.create(engine=model_engine,
+                                    prompt=prompt,
+                                    max_tokens=1024,
+                                    n=1,
+                                    stop=None,
+                                    temperature=0.7)
+respuesta=""
+for choice in completion.choices:
+    respuesta=respuesta+choice.text
+    print(f"Response: %s" % choice.text)
